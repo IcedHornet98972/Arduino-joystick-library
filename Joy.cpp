d7ad30c33178c2514f7b5fd62152d6ceb953a951
Yura Kaptar <102322958+IcedHornet98972@users.noreply.github.com>
2023-03-05 20:22:50 +0200

    Add files via upload

---
+++ Joy.cpp
@@ -0,0 +1,40 @@
+
+#include "Joy.h"
+
+Joy::Joy(int URX1, int URY1, int SW1){
+  _URX1 = URX1;
+  _URY1 = URY1;
+  _SW1 = SW1;
+  pinMode(SW1, INPUT_PULLUP);
+}
+
+int Joy::readURY(){
+  
+int s = analogRead(_URY1);
+
+return s;
+}
+int Joy::readURX(){
+
+int k = analogRead(_URX1);
+
+return k;
+}
+int Joy::readSW(){
+  
+int v = digitalRead(_SW1);
+
+return v;
+}
+
+int Joy::UryForservo(){
+
+return map(analogRead(_URY1), 0, 1023, 0, 180);
+  
+}
+
+int Joy::UrxForservo(){
+
+return map(analogRead(_URX1), 0, 1023, 0, 180);;
+  
+}
